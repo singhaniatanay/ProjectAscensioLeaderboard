f50c2024ad8e07e1b2fe6d33abe626b50e23b988
Monil Bansal <bansalmonil7@gmail.com>
2020-10-07 15:51:11 +0530

    atlas

--- Backend/DAO/database.py
+++ Backend/DAO/database.py
@@ -1,17 +1,16 @@
-import pyrebase
-import json
-from firebase_admin import credentials, auth
+from flask import Flask, jsonify
+from flask_pymongo import pymongo
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','Config'))
+from atlassconfig import url
 
 
+CONNECTION_STRING = url
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('database')
+user_collection = pymongo.collection.Collection(db, 'User')
+team_collection = pymongo.collection.Collection(db, 'Team')
 
 
-
-cred = credentials.Certificate("../Config/firebaseAdminConfig.json")
-fb = firebase_admin.initialize_app(cred)
-pb = pyrebase.initialize_app(json.load(open('../Config/firebaseConfig.json')))
-
-
-print("Hello World")
-print(fb)
-print(cred)
-print(pb)
+for x in team_collection.find():
+	print(x)
--- GetDataPython/leetCode_toCSV.py
+++ GetDataPython/leetCode_toCSV.py
@@ -3,6 +3,7 @@ from leetcode_scrape import getData
 import sys,os
 
 
+<<<<<<< HEAD
 def leetcode_scrape(team):
 	df = pd.read_csv('../Database/{}/UserDatabase.csv'.format(team))
 	userLeetcode_Datas = []
@@ -10,3 +11,19 @@ def leetcode_scrape(team):
 	    userLeetcode_Datas.append(getData(i))
 	df_leetcode = pd.DataFrame(userLeetcode_Datas)
 	df_leetcode.to_csv('../Database/{}/leetcode_user_data.csv'.format(team),index=False)
+||||||| parent of 3b40edc... backend
+    df_leetcode = pd.DataFrame(userLeetcode_Datas)
+    df_leetcode.to_csv('../../Database/leetcode_usr_data.csv',index=False)
+=======
+def leetcode_scrape(team):
+	print("##################     CHECK    ##########################")
+	print(os.listdir())
+	print("##################   END   ################################")
+	df = pd.read_csv('../Database/{}/UserDatabase.csv'.format(team))
+	userLeetcode_Datas = []
+	for i in df['LeetCode_User']:
+	    userLeetcode_Datas.append(getData(i))
+
+	df_leetcode = pd.DataFrame(userLeetcode_Datas)
+	df_leetcode.to_csv('../Database/{}/leetcode_user_data.csv'.format(team),index=False)
+>>>>>>> 3b40edc... backend
